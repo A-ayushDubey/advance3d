ab243ac826ea58a66e7b7ac72ee1a3fd9994137b
Ayush Dubey <dayush521@email.com>
2026-06-22 14:04:23 +0530

    fix server startup

--- start.sh
+++ start.sh
@@ -4,8 +4,9 @@ echo "PORT IS: $PORT"
 echo "DB_HOST IS: $DB_HOST"
 
 php artisan config:clear
-php artisan migrate --force
+php artisan migrate --force || true
+php artisan storage:link || true
 php artisan config:cache
 
 echo "Starting on port $PORT"
-exec php -S 0.0.0.0:$PORT -t public
+exec php artisan serve --host=0.0.0.0 --port=$PORT
