fcc100fd5eca7c8e20e1284548f22496dfb5c0fb
Ayush Dubey <dayush521@email.com>
2026-06-22 11:41:06 +0530

    simplest startup

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 php artisan migrate --force
 php artisan storage:link || true
 php artisan config:cache
-exec php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
+exec php artisan serve --host=0.0.0.0 --port=8000
