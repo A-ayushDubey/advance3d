a254848cd543251dd929fa04b9bb256f1f664982
Ayush Dubey <dayush521@email.com>
2026-07-29 11:08:46 +0530

    update start.sh

--- start.sh
+++ start.sh
@@ -4,24 +4,9 @@ echo "PORT IS: $PORT"
 echo "DB_HOST IS: $DB_HOST"
 
 php artisan config:clear
-php artisan migrate --force || true
+php artisan migrate --force
 php artisan storage:link || true
 php artisan config:cache
 
 echo "Starting on port $PORT"
 exec php artisan serve --host=0.0.0.0 --port=$PORT
-
-
-#!/bin/sh
-
-# echo "PORT IS: $PORT"
-# echo "DB_HOST IS: $DB_HOST"
-
-# php artisan config:clear
-# php artisan migrate --force || true
-# php artisan storage:link || true
-# php artisan config:cache
-
-# echo "Starting on port $PORT"
-# exec php artisan serve --host=0.0.0.0 --port=$PORT
-
